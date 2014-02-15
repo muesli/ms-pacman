b92a10c7b785e16e9c228e17338d322ab7a78d41
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-15 13:16:26 +0100

    * Renamed resulting backup file.

--- backup_config.sh
+++ backup_config.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # backup modified configuration files
-pacman -Qii | awk '/^MODIFIED/ {print $2}' | tar -n -czf config_backup.tar.gz -T -
+pacman -Qii | awk '/^MODIFIED/ {print $2}' | tar -n -czf backup_config.tar.gz -T -
