a48b3ec8e1c074124b55f662ca40ccfc78da8d1f
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-15 13:37:35 +0100

    * Create master backup archive.

--- backup_system.sh
+++ backup_system.sh
@@ -7,3 +7,9 @@
 
 ./backup_config.sh
 ./backup_packagelists.sh
+
+# create master backup archive
+tar -czf backup_system.tar.gz backup_config.tar.gz pkglist-off.txt pkglist-loc.txt
+
+# clean up
+rm backup_config.tar.gz pkglist-off.txt pkglist-loc.txt
