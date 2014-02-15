8f43183a44c4ac44429164357521edd0016a470b
Christian Muehlhaeuser <muesli@gmail.com>
2014-02-15 13:38:01 +0100

    * Restore system from master backup archive.

--- restore_system.sh
+++ restore_system.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# extract master backup archive
+tar xf backup_system.tar.gz
+
 ./restore_config.sh
 ./restore_packages.sh
+
+# clean up
+rm backup_config.tar.gz pkglist-off.txt pkglist-loc.txt
